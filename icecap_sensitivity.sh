494e60734863bd918f287a44d82200e837a9ae72
HaHoHe1892 <t.frank95@web.de>
2022-08-08 10:05:29 +0200

    icecap sensitivity ice temperature

--- icecap_sensitivity.sh
+++ icecap_sensitivity.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-for vel_factor in .25 .5 .75 1.25 1.5 1.75
+for ice_temp in 264 266 270 272
 do
-    mpiexec -n 1 python3 icecap_inversion.py -vel_factor $vel_factor
+    mpiexec -n 1 python3 icecap_inversion.py -ice_temp $ice_temp
 done
 
 
 : <<'END'
+
+def calc_ice_temps(T):
+    return 1.733e3*np.exp(-13.9e4/(8.3*T)
+
 import numpy as np
 from funcs import *
 from bed_inversion import *
@@ -15,17 +19,17 @@ from sklearn.linear_model import LinearRegression
 bed_deviations = []
 reference_bed = get_nc_data('icecap_output_mb_1.0.nc', 'topg', -1).data
 mask = get_nc_data('ice_build_output.nc', 'mask', -1).data/2
-vel_factors = [0.25, .5, .75, 1.25, 1.5, 1.75]
-for i in vel_factors:
-	 bed = get_nc_data('icecap_output_vel_{}.nc'.format(i), 'topg', -1).data
-	 bed_deviations.append(np.nanmean((bed[mask==1] - reference_bed[mask==1])))
+ice_temps = [264.0, 266.0, 268.0, 270.0, 272.0]
+for i in ice_temps:
+    bed = get_nc_data('icecap_output_ice_temp_{}.nc'.format(i), 'topg', -1).data
+    bed_deviations.append(np.nanmean((bed[mask==1] - reference_bed[mask==1])))
 
-vel_factors.append(1)
-bed_deviations.append(1)
-plt.scatter(vel_factors, bed_deviations)
+#ice_temps.append(268)
+#bed_deviations.append(1)
+plt.scatter(ice_temps, bed_deviations)
 plt.show()
 
-model = LinearRegression().fit(np.array(vel_factors).reshape((-1,1)), np.array(bed_deviations))
-print(model.score(np.array(vel_factors).reshape((-1,1)), np.array(bed_deviations)))
+model = LinearRegression().fit(np.array(ice_temps).reshape((-1,1)), np.array(bed_deviations))
+print(model.score(np.array(ice_temps).reshape((-1,1)), np.array(bed_deviations)))
 
 END
