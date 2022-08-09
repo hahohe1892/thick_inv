d98efb0327bbbf671c2977faf7a553d3d9597ed9
HaHoHe1892 <t.frank95@web.de>
2022-08-09 10:59:40 +0200

    figure for mass balance sensitivity as in paper. 5th power relationship and linear relationship are plotted

--- icecap_sensitivity.sh
+++ icecap_sensitivity.sh
@@ -18,6 +18,7 @@ bed_deviations_tf = []
 taucs = []
 vels = []
 vols = []
+vols_tf = []
 fluxes = []
 true_bed = get_nc_data('ice_build_output.nc', 'topg', -1)
 true_surf = get_nc_data('ice_build_output.nc', 'usurf', -1)
@@ -34,7 +35,9 @@ for i in xs:
     bed = get_nc_data('icecap_output_mb_{}.nc'.format(i), 'topg', -1)
     bed_tf = get_nc_data('icecap_output_mb_{}_true_friction.nc'.format(i), 'topg', -1)
     surf = get_nc_data('icecap_output_mb_{}.nc'.format(i), 'usurf', -1)
+    surf_tf =  get_nc_data('icecap_output_mb_{}_true_friction.nc'.format(i), 'usurf', -1)
     vols.append(np.sum(surf-bed))
+    vols_tf.append(np.sum(surf_tf-bed_tf))
     taucs.append(np.nanmean(get_nc_data('icecap_output_mb_{}.nc'.format(i), 'tauc', 0)[mask==1]))
     vels.append(np.nanmean(get_nc_data('icecap_output_mb_{}.nc'.format(i), 'velbar_mag', 0)[mask==1]))
     fluxes.append(np.sum(get_nc_data('icecap_output_mb_{}.nc'.format(i), 'flux_mag', 0)[mask==1]))
@@ -46,13 +49,22 @@ fig, ax = plt.subplots()
 taucs.append(np.mean(reference_tauc[mask==1]))
 vels.append(np.nanmean(reference_vel[mask==1]))
 vols.append(reference_vol)
+vols_tf.append(reference_vol)
 bed_deviations_tf.append(0)
 fluxes.append(np.sum(reference_flux))
-points1 = ax.scatter(xs,bed_deviations_tf, c = 'orange')
-ax1 = ax.twinx()
+points1 = ax.scatter(xs,vols_tf/reference_vol, c = 'orange', label='true friction field prescribed')
+line1 = ax.plot(np.sort(xs), np.sort(np.array(xs)**(1/5)), '--', c = 'orange', label='5th root of mass balance errors')
+#ax1 = ax.twinx()
 bed_deviations.append(0)
-points = ax.scatter(xs, bed_deviations)
+points = ax.scatter(xs, vols/reference_vol, label='friction updates are applied')
+line = ax.plot(np.sort(xs), np.sort(xs), '--', label='1:1 line')
+ax.legend()
+ax.set_xlabel('mass balance / true mass balance')
+ax.set_ylabel('modelled ice volume / true ice volume')
+plt.savefig('./figures/mass_balance_errors.png')
 plt.show()
+
+
 
 model = LinearRegression().fit(np.array(S_ref_rands).reshape((-1,1)), np.array(bed_deviations))
 print(model.score(np.array(S_ref_rands).reshape((-1,1)), np.array(bed_deviations)))
