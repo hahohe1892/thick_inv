f5abd9417b2cfafbb5bfd78f17d7928bbe435c74
HaHoHe1892 <t.frank95@web.de>
2022-08-11 11:22:12 +0200

    figure v.1 for velocity sensitivity

--- icecap_sensitivity.sh
+++ icecap_sensitivity.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ #!/bin/bash
 
 for S_ref_rand in 5 10 15 25 50
 do
@@ -40,46 +40,47 @@ reference_tauc = get_nc_data('icecap_output_mb_1.0.nc', 'tauc', -1)
 reference_vel  = get_nc_data('icecap_output_mb_1.0.nc', 'velbar_mag', -1)
 reference_flux = get_nc_data('icecap_output_mb_1.0.nc', 'flux_mag', -1)
 mask = get_nc_data('ice_build_output.nc', 'mask', -1).data/2
-xs = [5.0, 10.0, 15.0, 25.0, 50.0]
+xs = [0.25, 0.5, 0.75, 1.25, 1.5, 1.75]
 for i in xs:
-    bed = get_nc_data('icecap_output_S_ref_rand_{}_correct_diffusivity.nc'.format(i), 'topg', -1)
+    bed = get_nc_data('icecap_output_vel_{}.nc'.format(i), 'topg', -1)
     beds.append(bed)
-    surf = get_nc_data('icecap_output_S_ref_rand_{}_correct_diffusivity.nc'.format(i), 'usurf', -1)
+    surf = get_nc_data('icecap_output_vel_{}.nc'.format(i), 'usurf', -1)
     surfs.append(surf)
     surfs_in.append(np.maximum(true_surf + np.random.normal(0, x, np.shape(true_surf)), true_bed))
     vols.append(np.sum(surf-bed))
-    taucs.append(np.nanmedian(get_nc_data('icecap_output_S_ref_rand_{}_correct_diffusivity.nc'.format(i), 'tauc', 0)[mask==1]))
-    vels.append(np.nanmean(get_nc_data('icecap_output_S_ref_rand_{}_correct_diffusivity.nc'.format(i), 'velbar_mag', 0)[mask==1]))
-    fluxes.append(np.sum(get_nc_data('icecap_output_S_ref_rand_{}_correct_diffusivity.nc'.format(i), 'flux_mag', 0)[mask==1]))
+    taucs.append(np.nanmedian(get_nc_data('icecap_output_vel_{}.nc'.format(i), 'tauc', 0)[mask==1]))
+    vels.append(np.nanmean(get_nc_data('icecap_output_vel_{}.nc'.format(i), 'velbar_mag', 0)[mask==1]))
+    fluxes.append(np.sum(get_nc_data('icecap_output_vel_{}.nc'.format(i), 'flux_mag', 0)[mask==1]))
     bed_deviations.append(rmse(bed[mask==1], reference_bed[mask==1]))
     rsmes.append(rmse(bed[mask==1], true_bed[mask==1]))
 
-xs.append(0)
+
 taucs.append(np.median(reference_tauc[mask==1]))
 vels.append(np.nanmean(reference_vel[mask==1]))
-vols.append(reference_vol)
-vols_tf.append(reference_vol)
-bed_deviations_tf.append(0)
 fluxes.append(np.sum(reference_flux))
-bed_deviations.append(0)
+xs.append(1)
+vols.append(reference_vol)
+bed_deviations.append(rmse(reference_bed[mask==1], true_bed[mask==1]))
 
 fig, ax = plt.subplots()
+line = ax.plot(xs, [no_sliding_vol/true_vol]*len(xs), '--', c='black')
+plt.text(0.8, no_sliding_vol/true_vol+0.01, 'ice volume if no sliding anywhere')
 points = ax.scatter(xs, vols/true_vol, label = 'applying friction updates', c=bed_deviations, cmap='plasma')
-ax.set_xlabel('Std of random noise (m)')
+ax.set_xlabel('velocity / true velocity')
 ax.set_ylabel('modelled ice volume / true ice volume')
 cbar = fig.colorbar(points, ax = ax)
 cbar.ax.set_ylabel('RMSE')
 ax1 = plt.axes([0,0,1,1])
-ip1 = InsetPosition(ax, [0.1,0.6,0.3,0.3])
+ip1 = InsetPosition(ax, [0.1,0.5,0.3,0.3])
 ax1.set_axes_locator(ip1)
 ax1.pcolor(beds[0], vmin = -500, vmax = 1000)
 ax1.axis('off')
 ax2 = plt.axes([0,0,.99,1])
-ip2 = InsetPosition(ax, [0.6,0.1,0.3,0.3])
+ip2 = InsetPosition(ax, [0.65,0.4,0.3,0.3])
 ax2.set_axes_locator(ip2)
 ax2.pcolor(beds[-1], vmin = -500, vmax = 1000)
 ax2.axis('off')
-plt.savefig('./figures/surface_shape_errors.png')
+#plt.savefig('./figures/mass_balance_errors_v1.0.png')
 plt.show()
 
 model = LinearRegression().fit(np.array(S_ref_rands).reshape((-1,1)), np.array(bed_deviations))
